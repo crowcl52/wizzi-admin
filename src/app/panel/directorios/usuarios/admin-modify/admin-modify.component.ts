a1f66302c8b6d762af16fb665665ebfd410597af
inigo navarrete mercado <inigonavarrete@aori.mx>
2019-07-11 23:57:27 -0500

    peticion de datos

--- src/app/panel/directorios/usuarios/admin-modify/admin-modify.component.ts
+++ src/app/panel/directorios/usuarios/admin-modify/admin-modify.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-admin-modify',
@@ -7,8 +8,130 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminModifyComponent implements OnInit {
 
-  constructor() { }
+  GetClients: any;
+  GetClient: any;
+  GetAdmins: any;
+  GetAdmin: any;
+  GetWizis: any;
+  GetWizi: any;
+  aux: any;
+  options: boolean;
+  search: boolean;
+  GetClientServices: any;
+  GetWiziServices: any;
+  GetCar: any;
+  GetBrand: any;
+  GetModel: any;
+  constructor(private userService: UserService) {
 
+  }
+
+  GetClientsFunction(){
+    this.userService.GetClients().subscribe( (data: any) => {
+      console.log(data);
+      this.GetClients = data.data.items;
+    })
+  }
+  GetClientFunction(id){
+    this.userService.GetClient(id).subscribe( (data: any) => {
+      console.log(data);
+      this.GetClient = data.data.items;
+    })
+  }
+  GetAdminsFunction(){
+    this.userService.GetAdmins().subscribe( (data: any) => {
+      console.log(data);
+      this.GetAdmins = data.data.items;
+    })
+  }
+  GetAdminFunction(id){
+    this.userService.GetAdmin(this.GetAdmins.id).subscribe( (data: any) => {
+      console.log(data);
+      this.GetAdmin = data.data.items;
+    })
+  }
+  GetWizisFunction(){
+    this.userService.GetWizis().subscribe( (data: any) => {
+      console.log(data);
+      this.GetWizis = data.data.items;
+    })
+  }
+  GetWiziFunction(id){
+    this.userService.GetWizi(id).subscribe( (data: any) => {
+      console.log(data);
+      this.GetWizi = data.data.items;
+    })
+  }
+  GetClientServicesFunction(id){
+    this.userService.GetClientServices(id).subscribe( (data: any) => {
+      console.log(data);
+      this.GetClientServices = data.data.items;
+    })
+  }
+  GetWiziServicesFunction(id){
+    this.userService.GetWiziServices(this.GetWizis.id).subscribe( (data: any) => {
+      console.log(data);
+      this.GetWiziServices = data.data.items;
+    })
+  }
+  GetCarFunction(id){
+    this.userService.GetCar(id).subscribe( (data: any) => {
+      console.log(data);
+      this.GetCar = data.data.items;
+    })
+  }
+  GetBrandFunction(brand){
+    this.userService.GetBrand(brand).subscribe( (data: any) => {
+      console.log(data);
+      this.GetBrand = data.data.items;
+    })
+  }
+  GetModelFunction(model){
+    this.userService.GetModel(model).subscribe( (data: any) => {
+      console.log(data);
+      this.GetModel = data.data.items;
+    })
+  }
+  DeleteAdmin(id){
+    this.userService.DeleteAdmin(id).subscribe( (data: any) => {
+      console.log(data);
+    })
+  }
+  DeleteWizi(id){
+    this.userService.DeleteWizi(id).subscribe( (data: any) => {
+      console.log(data);
+    })
+  }
+  DeleteClient(id){
+    this.userService.DeleteClient(id).subscribe( (data: any) => {
+      console.log(data);
+    })
+  }
+  PatchClient(id, body){
+    this.userService.PatchClient(id, body).subscribe( (data: any) => {
+      console.log(data);
+    })
+  }
+  PatchWizi(id, body){
+    this.userService.PatchWizi(id, body).subscribe( (data: any) => {
+      console.log(data);
+    })
+  }
+  PatchAdmin(id, body){
+    this.userService.PatchAdmin(id, body).subscribe( (data: any) => {
+      console.log(data);
+    })
+  }
+  PostAdmin(body){
+    this.userService.PostAdmin(body).subscribe( (data: any) => {
+      console.log(data);
+    })
+  }
+  PostWizi(body){
+    this.userService.PostWizi(body).subscribe( (data: any) => {
+      console.log(data);
+    })
+  }
   ngOnInit() {
   }
 
--- src/app/panel/directorios/usuarios/client/client.component.ts
+++ src/app/panel/directorios/usuarios/client/client.component.ts
@@ -9,13 +9,131 @@ import { UserService } from '../user.service';
 export class ClientComponent implements OnInit {
   services: boolean;
   active: boolean;
+  GetClients: any;
+  GetClient: any;
   GetAdmins: any;
+  GetAdmin: any;
+  GetWizis: any;
+  GetWizi: any;
+  aux: any;
+  options: boolean;
+  search: boolean;
+  GetClientServices: any;
+  GetWiziServices: any;
+  GetCar: any;
+  GetBrand: any;
+  GetModel: any;
   constructor(private userService: UserService) {
     this.services = false;
     this.active = true;
+
+  }
+
+
+  GetClientsFunction(){
+    this.userService.GetClients().subscribe( (data: any) => {
+      console.log(data);
+      this.GetClients = data.data.items;
+    })
+  }
+  GetClientFunction(id){
+    this.userService.GetClient(id).subscribe( (data: any) => {
+      console.log(data);
+      this.GetClient = data.data.items;
+    })
+  }
+  GetAdminsFunction(){
     this.userService.GetAdmins().subscribe( (data: any) => {
       console.log(data);
       this.GetAdmins = data.data.items;
+    })
+  }
+  GetAdminFunction(id){
+    this.userService.GetAdmin(this.GetAdmins.id).subscribe( (data: any) => {
+      console.log(data);
+      this.GetAdmin = data.data.items;
+    })
+  }
+  GetWizisFunction(){
+    this.userService.GetWizis().subscribe( (data: any) => {
+      console.log(data);
+      this.GetWizis = data.data.items;
+    })
+  }
+  GetWiziFunction(id){
+    this.userService.GetWizi(id).subscribe( (data: any) => {
+      console.log(data);
+      this.GetWizi = data.data.items;
+    })
+  }
+  GetClientServicesFunction(id){
+    this.userService.GetClientServices(id).subscribe( (data: any) => {
+      console.log(data);
+      this.GetClientServices = data.data.items;
+    })
+  }
+  GetWiziServicesFunction(id){
+    this.userService.GetWiziServices(this.GetWizis.id).subscribe( (data: any) => {
+      console.log(data);
+      this.GetWiziServices = data.data.items;
+    })
+  }
+  GetCarFunction(id){
+    this.userService.GetCar(id).subscribe( (data: any) => {
+      console.log(data);
+      this.GetCar = data.data.items;
+    })
+  }
+  GetBrandFunction(brand){
+    this.userService.GetBrand(brand).subscribe( (data: any) => {
+      console.log(data);
+      this.GetBrand = data.data.items;
+    })
+  }
+  GetModelFunction(model){
+    this.userService.GetModel(model).subscribe( (data: any) => {
+      console.log(data);
+      this.GetModel = data.data.items;
+    })
+  }
+  DeleteAdmin(id){
+    this.userService.DeleteAdmin(id).subscribe( (data: any) => {
+      console.log(data);
+    })
+  }
+  DeleteWizi(id){
+    this.userService.DeleteWizi(id).subscribe( (data: any) => {
+      console.log(data);
+    })
+  }
+  DeleteClient(id){
+    this.userService.DeleteClient(id).subscribe( (data: any) => {
+      console.log(data);
+    })
+  }
+  PatchClient(id, body){
+    this.userService.PatchClient(id, body).subscribe( (data: any) => {
+      console.log(data);
+    })
+  }
+  PatchWizi(id, body){
+    this.userService.PatchWizi(id, body).subscribe( (data: any) => {
+      console.log(data);
+    })
+  }
+  PatchAdmin(id, body){
+    this.userService.PatchAdmin(id, body).subscribe( (data: any) => {
+      console.log(data);
+    })
+  }
+  PostAdmin(body){
+    this.userService.PostAdmin(body).subscribe( (data: any) => {
+      console.log(data);
+    })
+  }
+  PostWizi(body){
+    this.userService.PostWizi(body).subscribe( (data: any) => {
+      console.log(data);
     })
   }
 
